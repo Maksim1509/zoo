402024e090cb349f3a818d928917cfe7f868fcca
Maksim1509 <maaa23@mail.ru>
2022-10-07 20:22:40 +0400

    feat: implement testimonials carousel

--- online-zoo/pages/petstory/script.js
+++ online-zoo/pages/petstory/script.js
@@ -1,5 +1,7 @@
 import menuControler from "../../assets/js/menuControler.js";
 import carousel from '../..//assets/js/carousel.js';
+import testimonial from '../../assets/js/testimonial.js';
 
 menuControler();
 carousel();
+testimonial();
