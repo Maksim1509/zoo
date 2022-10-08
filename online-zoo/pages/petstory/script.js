d44f2c37411e2dfd6a68c6bd81f10f2bf784b235
Maksim1509 <maaa23@mail.ru>
2022-10-08 21:11:10 +0400

    feat: implement popap

--- online-zoo/pages/petstory/script.js
+++ online-zoo/pages/petstory/script.js
@@ -1,7 +1,9 @@
 import menuControler from "../../assets/js/menuControler.js";
 import carousel from '../..//assets/js/carousel.js';
 import testimonial from '../../assets/js/testimonial.js';
+import popap from '../../assets/js/popap.js';
 
 menuControler();
 carousel();
 testimonial();
+popap();
